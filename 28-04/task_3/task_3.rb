2b5a75d043831c8ff9239702f44f4938252d59ad
Yuliya <cisancisan11@gmail.com>
2020-05-20 12:49:57 +0300

    Add user input and file closing

--- 28-04/task_3/task_3.rb
+++ 28-04/task_3/task_3.rb
@@ -1,8 +1,11 @@
 rjust_text_buffer = ""
+print "Enter the number of spaces: "
+spaces_num = gets.to_i
 
 File.open("f3.txt").each do |line|
-  rjust_text_buffer += line.rjust(50)
+  rjust_text_buffer += line.rjust(spaces_num + line.size)
 end
 
 rjust_strings_file = File.open("g3.txt","w+")
 rjust_strings_file.write rjust_text_buffer
+rjust_strings_file.close
