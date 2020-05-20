5c6eeabf6b9fc8552410e0ec407528ba622bf740
Yuliya <cisancisan11@gmail.com>
2020-05-20 20:44:29 +0300

    Refactoring task_2.rb(01-05) and add README.md

---
+++ 01-05/task_2/task_2.rb
@@ -0,0 +1,15 @@
+integ_nums1 = []
+
+File.open("nums1.txt").each do |line1|
+  integ_nums1 = line1.split
+end
+
+integ_nums2 = []
+
+File.open("nums2.txt").each do |line2|
+  integ_nums2 = line2.split
+end
+
+united_nums_file = File.open("nums-common.txt", "w+")
+united_nums_file.write (integ_nums1 & integ_nums2).join(", ")
+united_nums_file.close
