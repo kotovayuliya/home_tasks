de949d72bec241e9dabf91240169b41af9e76709
Yuliya <cisancisan11@gmail.com>
2020-05-25 15:21:51 +0300

    Code reduction

--- 10-05/task_2/task2.rb
+++ 10-05/task_2/task2.rb
@@ -1,13 +1,3 @@
-parag_counter = 0
-
-lines_with_break_symb = File.readlines("parag1.txt")
-text = lines_with_break_symb.join
-lines_without_break_symb = text.split(/\n/)
-
-lines_without_break_symb.each do |string|
-  if /.+/.match(string)
-    parag_counter += 1
-  end
-end
+parag_counter = File.readlines("parag1.txt").count { |line| line =~ /\S+\n$/ }
 
 puts parag_counter
