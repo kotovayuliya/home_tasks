36d17e48233bead7dae517b31fb611fe5ec37a2d
Yuliya <cisancisan11@gmail.com>
2020-05-25 20:36:24 +0300

    Delete redundant line, correction

--- 10-05/task_5/task5.rb
+++ 10-05/task_5/task5.rb
@@ -6,7 +6,6 @@ text_buffer = text_lines.join
 all_elements_counter = text_buffer.length
 puts "Number of symbols: #{all_elements_counter}"
 
-# symbols_without_spaces = text_buffer.gsub(/\s|\t|\n|\r/, '').length
 spaces_counter = text_buffer.scan(/\s|\t|\n|\r/).count
 percentage = (spaces_counter.to_f / all_elements_counter.to_f) * 100
 puts "Space percentage: #{percentage}"
