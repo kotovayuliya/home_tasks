b8a969f5dc2af01528e367788f7f78bd91134007
Yuliya <cisancisan11@gmail.com>
2020-05-25 20:41:11 +0300

    Code reduction

--- 10-05/task_6/task6.rb
+++ 10-05/task_6/task6.rb
@@ -1,9 +1,4 @@
-punct_marks = ""
-
-File.open("symbols.txt").each do |line|
-  text_and_spaces = line.scan(/\w+\s*/).join
-  punct_marks << line.delete(text_and_spaces)
-end
+punct_marks = File.read("symbols.txt").gsub(/[\w\s]+/, '')
 
 punct_marks_file = File.open("symbols-1.txt", "w+")
 punct_marks_file.write punct_marks
