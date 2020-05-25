93939562fa215c539c89716827ec356808e17371
Yuliya <cisancisan11@gmail.com>
2020-05-25 19:04:48 +0300

    Code reduction

--- 10-05/task_5/task5.rb
+++ 10-05/task_5/task5.rb
@@ -1,16 +1,12 @@
-text_buffer = ""
+text_lines = File.readlines('statistic.txt')
+puts "Number of lines: #{text_lines.size}"
 
-strings_counter = File.readlines("statistic.txt").count
-puts "Number of lines: #{strings_counter}"
-
-File.open("statistic.txt").each do |line|
-  text_buffer << line
-end
+text_buffer = text_lines.join
 
 all_elements_counter = text_buffer.length
 puts "Number of symbols: #{all_elements_counter}"
 
 symbols_without_spaces = text_buffer.gsub(/\s|\t|\n|\r/, '').length
-spaces_counter = all_elements_counter - symbols_without_spaces
+spaces_counter = text_buffer.count(" ")
 percentage = (spaces_counter.to_f / all_elements_counter.to_f) * 100
 puts "Space percentage: #{percentage}"
